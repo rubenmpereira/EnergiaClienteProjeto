87d911adda4bb305d0f1268d3114938865b71073
Ruben Pereira <rubenpereira205@gmail.com>
2024-01-23 17:30:40 +0000

    Add Database class Add EnergiaClienteDominio project GetInvoices Database request GetInvoices Controller request GetInvoices Domain object

---
+++ EnergiaCliente/EnergiaCliente/EnergiaClienteController.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using EnergiaClienteDados;
+using EnergiaClienteDominio;
+using EnergiaClienteDados.RequestModels;
+using Grpc.Core;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EnergiaCliente
+{
+    public class EnergiaClienteController
+    {
+        private readonly ILogger _logger;
+
+        public EnergiaClienteController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<EnergiaClienteController>();
+        }
+
+        [Function("GetInvoices")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            Database database = new Database();
+
+            var data = database.GetInvoices(new GetInvoicesRequestModel() { habitacao = 1 });
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            var jsonToReturn = JsonConvert.SerializeObject(data);
+
+            response.WriteString(jsonToReturn);
+
+            return response;
+        }
+    }
+}
---
+++ EnergiaCliente/EnergiaClienteDados/Database.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using EnergiaClienteDados.RequestModels;
+using EnergiaClienteDominio;
+using System.Reflection;
+using System.Text;
+
+namespace EnergiaClienteDados
+{
+    public class Database
+    {
+        private SqlConnection connection = new SqlConnection("Data Source=MSIRUBEN\\SQLEXPRESS;Initial Catalog=EnergiaClienteDados;Integrated Security=True;TrustServerCertificate=True");
+
+        public List<Invoice> GetInvoices(GetInvoicesRequestModel requestModel)
+        {
+            //set parameters
+            var param = new SqlParameter("habitacao", requestModel.habitacao);
+
+            //execute stored procedure
+            var response = RunSelectProcedure("UltimasFaturas", new SqlParameter[1] { param });
+
+            //mapping
+            var invoices = new List<Invoice>();
+            foreach (DataRow row in response)
+            {
+                var invoice = new Invoice();
+                invoice.number = row["numero"].ToString();
+                invoice.startDate = DateTime.Parse(row["dataInicio"].ToString());
+                invoice.endDate = DateTime.Parse(row["dataFim"].ToString());
+                invoice.Paid = bool.Parse(row["pago"].ToString());
+                invoice.Value = decimal.Parse(row["valor"].ToString());
+                invoice.limitDate = DateTime.Parse(row["dataLimite"].ToString());
+                invoice.habitation = int.Parse(row["idHabitacao"].ToString());
+                invoice.document = Encoding.ASCII.GetBytes(row["documento"].ToString());
+                invoices.Add(invoice);
+            }
+
+            return invoices;
+        }
+
+        private int Run(SqlCommand command)
+        {
+            command.Connection = connection;
+            return command.ExecuteNonQuery();
+        }
+
+        private DataRowCollection RunGet(SqlCommand command)
+        {
+            command.Connection = connection;
+            var dataAdapter = new SqlDataAdapter(command);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            if (dataSet.Tables[0].Rows.Count == 0)
+                return null;
+
+            return dataSet.Tables[0].Rows;
+        }
+
+        private DataRowCollection RunSelectProcedure(string procedure, SqlParameter[] parameters)
+        {
+            var dataAdapter = new SqlDataAdapter(procedure, connection);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            dataAdapter.SelectCommand.Parameters.AddRange(parameters);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            if (dataSet.Tables[0].Rows.Count == 0)
+                return null;
+
+            return dataSet.Tables[0].Rows;
+        }
+    }
+}
