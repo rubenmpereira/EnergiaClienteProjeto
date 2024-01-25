a2cd3ae463499b4fe8387cd6f4c19edd6984086e
Ruben Pereira <rubenpereira205@gmail.com>
2024-01-25 15:38:47 +0000

    Add GetReadings calls UltimasLeiturasSP

---
+++ EnergiaCliente/EnergiaClienteDominio/Reading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergiaClienteDominio
+{
+    public class Reading
+    {
+        public int id { get; set; }
+        public int vazio { get; set; }
+        public int ponta { get; set; }
+        public int cheias { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+        public DateTime readingDate { get; set; }
+        public int habitation { get; set; }
+        public bool estimated { get; set; }
+    }
+}
